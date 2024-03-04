caceb05d804eac698d2dcd068941268b2a5b7173
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-03-04 17:39:38 +0100

    Update Esercizio3.java

--- day1/src/Esercizio3.java
+++ day1/src/Esercizio3.java
@@ -6,27 +6,21 @@ public class Esercizio3 {
         System.out.println("Il perimetro del rettangolo è: " +  perimentroRettangolo);
         int numberPari = pariDispari(55);
         System.out.println(numberPari);
-       // double Erone = formulaDiErone(20.45, 45.10, 25.67);
-        //System.out.println("Questo è il risultato del calcolo del perimetro del triangolo secondo la formula di Erone: " + Math.sqrt(Erone) % 4);
 
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lato1:");
+        double num4 = Double.parseDouble(scanner.nextLine());
+        System.out.println("lato2:");
+        double num5 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Lato3:");
+        double num6 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Il risultato è: " +  (Math.sqrt( (num4 + num5 + num6) * (-num4 + num5 + num6) * (num4 - num5 + num6) * (num4 + num5 - num6)) / 4));
+        scanner.close();
     }
     public static double perimetro(double num1, double num2) {
         return num1 * 2 + num2 * 2;
     }
     public static int pariDispari(int num3) {
-        return num3 % 2 == 0 ? 0 : 1;
+        return num3 / 2 == 0 ? 0 : 1;
     }
-    public static void formulaDiErone(double[] args){
-    Scanner scanner = new Scanner(System.in);
-        System.out.println("Lato1:");
-        String num4 = scanner.nextLine();
-        System.out.println("lato2:");
-        String num5 = scanner.nextLine();
-        System.out.println("Lato3:");
-        String num6 = scanner.nextLine();
-        System.out.println(Math.sqrt( (num4 + num5 + num6) * ((num4 + num5 + num6) -  num4) * ((num4 + num5 + num6) - num5 ) * ( (num4 + num5 + num6) - num6 )) % 4);
-        scanner.close();
-    }
-
 }
